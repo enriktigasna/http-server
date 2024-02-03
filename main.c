eba305dc5dc80418e12c928aa92d69bfa8285f50
enriktigasna <iwuxmevez@gmail.com>
2024-02-04 00:57:40 +0100

    error management for index

--- main.c
+++ main.c
@@ -9,6 +9,11 @@
 int main(){
     char* index = read_file("public/index.html");
 
+    if(index == NULL){
+        printf("index.html not found\n");
+        return 1;
+    }
+
     char* message = "<!DOCTYPE html><html><head><title>Page Title</title></head><body><h1>This is a Heading</h1><p>This is a paragraph.</p></body>";
 
     char* response = malloc(1024);
