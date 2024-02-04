436b822e9b9ca08c981031361dcba3a6e3811569
enriktigasna <maxim.pavlenko2011@gmail.com>
2024-02-04 03:13:22 +0100

    Finished router
    Only left is to add response codes so that 404 will give 404 instead of 200

--- main.c
+++ main.c
@@ -8,22 +8,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(){
-    char* index = read_file("public/index.html");
+#define ROOT_DIR "public"
+#define MAX_PATH 100
 
-    if(index == NULL){
-        printf("index.html not found\n");
-        return 1;
-    }
+char* notFound(){
+    char* path = malloc(MAX_PATH);
+    sprintf(path, "%s%s", ROOT_DIR, "/404.html");
+
+    return read_file(path) ? read_file(path) : "HTTP/1.1 404 Not Found\n";
+}
+
+int main(){
+    char* path = malloc(MAX_PATH);
 
     char* response = malloc(1024);
     int socket = create_socket(8080);
 
+    // Router
     while((response = socket_read(socket)) != NULL){
         struct headers* headers = parse_headers(response);
         printf("%s %s FROM %s\n", headers->method, headers->path, headers->user_agent);
 
-        socket_send(socket, index);
+        if(strcmp(headers->path, "/") == 0 || strcmp(headers->path, "/index.html") == 0){
+            sprintf(path, "%s%s", ROOT_DIR, "/index.html");
+            socket_send(socket, read_file(path));
+        } else {
+            sprintf(path, "%s%s", ROOT_DIR, headers->path);
+            socket_send(socket, read_file(path) ? read_file(path) : notFound());
+        }
     }
 
     return 0;
